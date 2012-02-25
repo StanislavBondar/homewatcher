bfc440b21607773b9e8d04e13a7f844d52ba1fbb
Donn Renk <drenkg@gmail.com>
2012-02-24 19:02:33 -0600

    New Fragment Code

---
+++ HomeWatcher/src/com/donn/homewatcher/FragmentListener.java
@@ -0,0 +1,8 @@
+package com.donn.homewatcher;
+
+public interface FragmentListener {
+	
+    public void logActivity(String logString);
+    public void setSignedIn(boolean signedIn);
+
+}
---
+++ HomeWatcher/src/com/donn/homewatcher/LoggingTabFragment.java
@@ -0,0 +1,99 @@
+package com.donn.homewatcher;
+
+import com.donn.envisalink.communication.PanelException;
+import com.donn.envisalink.tpi.SecurityPanel;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.SupportActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoggingTabFragment extends Fragment {
+	
+	private Button runCommandButton;
+	private RunCommandThread panelConnectionThread;
+	
+	private FragmentListener logListener;
+
+    @Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setRetainInstance(true);
+	}
+
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {        
+    	// Inflate the layout for this fragment
+		View view = inflater.inflate(R.layout.log_tab_fragment, container, false);
+		
+		runCommandButton = (Button) view.findViewById(R.id.button_run_command);
+		runCommandButton.setOnClickListener(new RunCommandButtonListener());
+
+		return view;
+    }
+	
+	@Override
+	public void onAttach(SupportActivity activity) {
+		super.onAttach(activity);
+		
+        try {
+            logListener = (FragmentListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString() + " must implement onActivityLogged");
+        }
+		
+		System.out.println("LOGTAB FRAGMENT: ATTACHED");
+	}
+
+	@Override
+	public void onDetach() {
+		super.onDetach();
+		
+		System.out.println("LOGTAB FRAGMENT: DETACHED");
+	}
+	
+	private class RunCommandButtonListener implements OnClickListener {
+
+		public void onClick(View v) {
+			EditText editText = (EditText) getView().findViewById(R.id.edit_command_to_run);
+			String command = editText.getText().toString();
+			panelConnectionThread = new RunCommandThread(command);
+			panelConnectionThread.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, (Void[])null);
+		}
+	}
+	
+	private class RunCommandThread extends AsyncTask<Void, Void, Void> {
+
+		private String command;
+		
+		public RunCommandThread(String command) {
+			this.command = command;
+		}
+		
+		protected Void doInBackground(Void...args) {
+			
+			SecurityPanel panel = SecurityPanel.getSecurityPanel();
+			
+			logListener.logActivity("Command being executed: " + command);
+
+			try {
+				panel.runRawCommand(command);
+				logListener.logActivity("Command: " + command + " execute complete...");
+			}
+			catch (PanelException e) {
+				logListener.logActivity(e.getMessage());
+				e.printStackTrace();
+			}
+
+			return null;
+		}
+
+	}
+
+}
